be4577352d5c05b9da4da5add49944181a17497f
aloelsafzi <samsulfauzialoel@gmail.com>
2024-01-06 12:28:12 +0700

    fix background style

--- src/app/components/Main.jsx
+++ src/app/components/Main.jsx
@@ -1,18 +1,7 @@
-import Image from "next/image"
-
 const Main = ({ children, className }) => {
   return (
     <>
       <main className={`${className} mx-3`}>
-        <div className="absolute -z-10 h-screen w-11/12">
-          <Image
-            src="/bg-line.svg"
-            alt="background image"
-            layout="fill"
-            objectFit="cover"
-            quality={100}
-          />
-        </div>
         {children}
       </main>
     </>
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Image from "next/image";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,7 +12,20 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="id">
-      <body className={`${inter.className} container mx-auto`}>{children}</body>
+      <body className={`${inter.className}`}>
+        <div className="absolute -z-10 h-full w-full">
+          <Image
+            src="/bg-line.svg"
+            alt="background image"
+            layout="fill"
+            objectFit="cover"
+            quality={100}
+          />
+        </div>
+        <div className='container mx-auto'>
+          {children}
+        </div>
+      </body>
     </html>
   )
 }
