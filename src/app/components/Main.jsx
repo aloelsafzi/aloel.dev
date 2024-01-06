2a828e515360387bc7653d20134df4625e6d1212
aloelsafzi <samsulfauzialoel@gmail.com>
2024-01-06 12:06:33 +0700

    add background content

--- src/app/components/Main.jsx
+++ src/app/components/Main.jsx
@@ -1,9 +1,20 @@
-const Main = ({children, className}) => {
+import Image from "next/image"
+
+const Main = ({ children, className }) => {
   return (
     <>
-    <main className={`${className} mx-3`}>
-      {children}
-    </main>
+      <main className={`${className} mx-3`}>
+        <div className="absolute -z-10 h-screen w-11/12">
+          <Image
+            src="/bg-line.svg"
+            alt="background image"
+            layout="fill"
+            objectFit="cover"
+            quality={100}
+          />
+        </div>
+        {children}
+      </main>
     </>
   )
 }
